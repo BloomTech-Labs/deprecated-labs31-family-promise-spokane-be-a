2cdd12bf6572a04ba8e56234c7fbd16c63318382
Leslie Rodriguez <rleslie1015@gmail.com>
2020-12-14 14:32:38 -0600

    removes unneeded endpoint

--- api/guestLogs/logsModel.js
+++ api/guestLogs/logsModel.js
@@ -13,12 +13,6 @@ const findById = async (id) => {
   return db('logs').where({ id }).first().select('*');
 };
 
-//a function to find ALL logs by user id
-// needs work
-const findByUserId = async (user_id) => {
-  return db('logs').where({ user_id }).first().select('*');
-};
-
 const create = async (log) => {
   return db('logs').insert(log).returning('*');
 };
