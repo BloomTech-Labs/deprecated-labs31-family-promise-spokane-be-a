c6dd4a8cfe6edb9d56a125a9b48ceda62387ef26
Leslie Rodriguez <rleslie1015@gmail.com>
2020-12-14 07:30:43 -0600

    Revert "Revert "formatting documents""
    This reverts commit 08fc872b5aa10b524e3fc93d2849bf83a095bbb5.

--- api/notes/notesRouter.js
+++ api/notes/notesRouter.js
@@ -14,4 +14,89 @@ router.get('/', function (req, res) {
     });
 });
 
+router.get('/:id', function (req, res) {
+  const family_id = String(req.params.id);
+  Notes.findById(family_id)
+    .then((notes) => {
+      if (notes) {
+        res.status(200).json(notes);
+      } else {
+        res.status(404).json({ error: 'notes NotFound' });
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({ error: err.message });
+    });
+});
+
+router.post('/', async (req, res) => {
+  const notes = req.body;
+  if (notes) {
+    const id = notes['family_id'] || 0;
+    try {
+      await Notes.findByfamilyId(id).then(async (pf) => {
+        if (pf) {
+          //notes not found so lets insert it
+          await Notes.create(notes).then((notes) =>
+            res.status(200).json({ message: 'notes created', notes: notes[0] })
+          );
+        } else {
+          res.status(400).json({ message: 'family id doesnt exist!' });
+        }
+      });
+    } catch (e) {
+      console.error(e);
+      res.status(500).json({ message: e.message });
+    }
+  } else {
+    res.status(404).json({ message: 'notes missing' });
+  }
+});
+
+router.put('/', (req, res) => {
+  const notes = req.body;
+  if (notes) {
+    const id = notes['family_id'] || 0;
+    Notes.indById(id)
+      .then(
+        Notes.update(id, notes)
+          .then((updated) => {
+            res
+              .status(200)
+              .json({ message: 'notes created', notes: updated[0] });
+          })
+          .catch((err) => {
+            res.status(500).json({
+              message: `Could not update notes '${id}'`,
+              error: err.message,
+            });
+          })
+      )
+      .catch((err) => {
+        res.status(404).json({
+          message: `Could not find notes '${id}'`,
+          error: err.message,
+        });
+      });
+  }
+});
+
+router.delete('/:id', (req, res) => {
+  const id = req.params.id;
+  try {
+    Notes.findById(id).then((notes) => {
+      Notes.remove(notes['family_id']).then(() => {
+        res
+          .status(200)
+          .json({ message: `notes '${id}' was deleted.`, notes: notes });
+      });
+    });
+  } catch (err) {
+    res.status(500).json({
+      message: `Could not delete notes with ID: ${id}`,
+      error: err.message,
+    });
+  }
+});
+
 module.exports = router;
