f4e102b9254481498db422bb1843bda1065af917
Leslie Rodriguez <rleslie1015@gmail.com>
2020-12-15 11:10:26 -0600

    removes old comment

--- data/migrations/20201209123909_users.js
+++ data/migrations/20201209123909_users.js
@@ -3,7 +3,6 @@ exports.up = function (knex) {
   return knex.schema.createTable('users', (tbl) => {
     tbl.increments();
 
-    // TODO: check with isaiah to see if this username can stay without breaking Okta Auth
     tbl.string('username', 128);
 
     tbl.string('name');
