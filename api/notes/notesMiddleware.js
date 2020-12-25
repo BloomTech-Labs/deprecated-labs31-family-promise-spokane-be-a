73309e4815965cbd4725b07c8a1ef1e1cdbd26be
Isaiah Fowler <greatpup1940@gmail.com>
2020-12-25 01:02:40 -0500

    Created middleware that verifies if a note & family exists

--- api/notes/notesMiddleware.js
+++ api/notes/notesMiddleware.js
@@ -1,4 +1,6 @@
 const { roles } = require('./notesRoles');
+const Notes = require('./notesModel');
+const Families = require('../families/familiesModel');
 
 exports.grantAccess = function (action, resource) {
   return async (req, res, next) => {
@@ -14,4 +16,41 @@ exports.grantAccess = function (action, resource) {
       next(error);
     }
   };
+};
+
+exports.verifyNoteExists = async (req, res, next) => {
+  const { id } = req.params;
+
+  try {
+    const note = await Notes.findById(id);
+
+    if (!note) {
+      return res
+        .status(404)
+        .json({ message: `Note with id of ${id} does not exist` });
+    }
+
+    next();
+  } catch {
+    res.status(500).json({ message: 'Internal server error' });
+  }
+};
+
+exports.verifyFamilyExists = async (req, res, next) => {
+  const family_id = req.body['family_id'] || 0;
+
+  try {
+    const family = await Families.findById(family_id);
+
+    if (!family) {
+      return res
+        .status(404)
+        .json({ message: `Family with id of ${family_id} does not exist` });
+    }
+
+    next();
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: 'Internal server error' });
+  }
 };
--- api/notes/notesRouter.js
+++ api/notes/notesRouter.js
@@ -2,7 +2,7 @@ const express = require('express');
 const Notes = require('./notesModel');
 const authRequired = require('../middleware/authRequired');
 const router = express.Router();
-const Families = require('../families/familiesModel');
+const { verifyNoteExists, verifyFamilyExists } = require('./notesMiddleware');
 
 router.get('/', authRequired, async (req, res) => {
   try {
@@ -14,69 +14,38 @@ router.get('/', authRequired, async (req, res) => {
   }
 });
 
-router.get('/:id', authRequired, function (req, res) {
+router.get('/:id', authRequired, verifyNoteExists, async (req, res) => {
   const { id } = req.params;
-  Notes.findById(id)
-    .then((notes) => {
-      if (notes) {
-        res.status(200).json(notes);
-      } else {
-        res.status(404).json({ error: 'notes Not Found' });
-      }
-    })
-    .catch((err) => {
-      res.status(500).json({ error: err.message });
-    });
-});
-
-router.post('/', authRequired, async (req, res) => {
-  const newNote = req.body;
-
-  const id = newNote['family_id'] || 0;
 
   try {
-    // Verify if family exists
+    const note = await Notes.findById(id);
 
-    const family = await Families.findById(id);
-
-    if (!family) {
-      return res
-        .status(404)
-        .json({ message: `Family with id of ${id} does not exist` });
-    }
-
-    // Create note
-
-    let note = await Notes.create(newNote);
-
-    note = note[0];
-
-    res.status(201).json({ note });
-  } catch {
+    res.status(200).json({ note });
+  } catch (error) {
     res.status(500).json({ message: 'Internal server error' });
   }
 });
 
-router.put('/:id', authRequired, async (req, res) => {
+router.post('/', authRequired, verifyFamilyExists, async (req, res) => {
+  const newNote = req.body;
+
+  try {
+    const note = await Notes.create(newNote);
+
+    res.status(201).json({ note: note[0] });
+  } catch {
+    res.status(500).json({ message: ' server error' });
+  }
+});
+
+router.put('/:id', authRequired, verifyNoteExists, async (req, res) => {
   const updatedNote = req.body;
   const { id } = req.params;
 
   try {
-    // Update note
-
     let note = await Notes.findByIdAndUpdate(id, updatedNote);
 
-    note = note[0];
-
-    // If nothing is returned, the note doesn't exist
-
-    if (!note) {
-      return res
-        .status(404)
-        .json({ message: `Note with id of ${id} does not exist` });
-    }
-
-    res.status(200).json({ note });
+    res.status(200).json({ note: note[0] });
   } catch {
     res.status(500).json({
       message: `Could not update note with id of '${id}'`,
@@ -84,18 +53,10 @@ router.put('/:id', authRequired, async (req, res) => {
   }
 });
 
-router.delete('/:id', authRequired, async (req, res) => {
+router.delete('/:id', authRequired, verifyNoteExists, async (req, res) => {
   const { id } = req.params;
   try {
-    let note = await Notes.findByIdAndRemove(id);
-
-    note = note[0];
-
-    if (!note) {
-      return res
-        .status(404)
-        .json({ message: `Note with id of ${id} does not exist` });
-    }
+    await Notes.findByIdAndRemove(id);
 
     res.status(204).json({ message: 'Deleted' });
   } catch {
